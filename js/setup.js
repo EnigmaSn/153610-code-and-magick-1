0007fd9e720d72c00175163dd4e11d0f5ddf384b
Ponomareva <APonomareva@luxoft.com>
2017-11-28 18:44:44 +0300

    заполнила массивы

---
+++ js/setup.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+var firstName = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var lastName = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+var name = [1];
+console.log(name);
+
+for (var i = 0; i > lastName.length; i++) {
+  name[i] = firstName[i] + lastName[i];
+  console.log('Полное имя' + name);
+}
+
+var coatColor = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var eyesColor = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+var arr = [name, coatColor, eyesColor];
