669472a1885526a4a607874bf3b2cecbfd6f9181
EnigmaSn <enigmasnape@gmail.com>
2017-11-29 00:32:28 +0300

    Вывод данных по каждому волшебнику в консоль

--- js/setup.js
+++ js/setup.js
@@ -2,6 +2,7 @@
 
 var setup = document.querySelector('.setup');
 setup.classList.remove('hidden');
+
 var firstName = [
   'Иван',
   'Хуан Себастьян',
@@ -22,17 +23,8 @@ var lastName = [
   'Нионго',
   'Ирвинг'
 ];
-
-var wizardName = [];
-
-
-for (var i = 0; i < lastName.length; i++) {
-  var newName = firstName[i] + ' ' + lastName[i];
-  wizardName.push(newName);
-}
-console.log('Полные имена: ' + wizardName);
-
-var coatColor = [
+var wizardsNames = [];
+var coatColors = [
   'rgb(101, 137, 164)',
   'rgb(241, 43, 107)',
   'rgb(146, 100, 161)',
@@ -40,8 +32,7 @@ var coatColor = [
   'rgb(215, 210, 55)',
   'rgb(0, 0, 0)'
 ];
-
-var eyesColor = [
+var eyesColors = [
   'black',
   'red',
   'blue',
@@ -49,7 +40,35 @@ var eyesColor = [
   'green'
 ];
 
-var arr = [name, coatColor, eyesColor];
+var name;
+var coatColor;
+var eyesColor;
+//var arr = [name, coatColor, eyesColor];
+
+function getRandomElement(min, max) {
+  var randomElement = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomElement;
+}
+
+
+for (var i = 0; i < firstName.length; i++) {
+  var newName = firstName[i] + ' ' + lastName[i];
+  wizardsNames.push(newName);
+
+  //тут вывести рандомные функции
+  var randomWizard = getRandomElement(0, wizardsNames.length - 1);
+  var randomCoatColor = getRandomElement(0, coatColors.length - 1);
+  var randomEyesColor = getRandomElement(0, eyesColors.length - 1);
+
+  name = wizardsNames[randomWizard];
+  coatColor = coatColors[randomCoatColor];
+  eyesColor = eyesColors[randomEyesColor];
+
+  console.log('Полное именя: ' + name);
+  console.log('Цвет пальто: ' + coatColor);
+  console.log('Цвет глаз: ' + eyesColor);
+}
+
 
 // На основе данных, созданных в предыдущем пункте и шаблона #similar-wizard-template создайте DOM-элементы, соответствующие случайно сгенерированным волшебникам и заполните их данными из массива:
 //
